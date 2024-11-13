f95d2b8effb92b758644f736125b15f9950b30d1
Philip Rozycki-Burn <p.rozyckiburn@icloud.com>
2024-11-13 20:40:01 +0000

    initial convert lambda to TS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import pluginJs from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { ignores: ['dist/**', 'node_modules/**'] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
