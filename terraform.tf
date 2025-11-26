8f38adf7d20b1b1650c89a93c2d0be5583799628
Philip Rozycki-Burn <p.rozyckiburn@icloud.com>
2025-11-26 21:40:22 +0000

    upgrade tf providers

--- terraform.tf
+++ terraform.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0.1"
+      version = "~> 3.6.2"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.38.0"
+      version = "~> 6.0"
     }
     archive = {
       source  = "hashicorp/archive"
-      version = "~> 2.4.2"
+      version = "~> 2.7.1"
     }
   }
 
